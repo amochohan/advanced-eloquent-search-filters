8f09507035e21cfb332fa1b490f36c3a24f8b402
Amo Chohan <amo.chohan@gmail.com>
2016-05-16 13:53:25 +0100

    Implement UserSearch class

--- app/UserSearch/UserSearch.php
+++ app/UserSearch/UserSearch.php
@@ -2,12 +2,48 @@
 
 namespace App\UserSearch;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 class UserSearch
 {
     public static function apply(Request $filters)
     {
+        $query = static::applyDecoratorsFromRequest($filters, (new User)->newQuery());
 
+        return static::getResults($query);
+    }
+
+    private static function applyDecoratorsFromRequest(Request $request, Builder $query)
+    {
+        foreach ($request->all() as $filterName => $value) {
+
+            $decorator = static::createFilterDecorator($filterName);
+
+            if (static::isValidDecorator($decorator)) {
+                $query = $decorator::apply($query, $value);
+            }
+
+        }
+        return $query;
+    }
+
+    private static function createFilterDecorator($name)
+    {
+        return sprintf('%\\Filters\\%s',
+            __NAMESPACE__,
+            str_replace(' ', '', ucwords(str_replace('_', ' ', $name)))
+        );
+    }
+
+    private static function isValidDecorator($decorator)
+    {
+        return class_exists($decorator);
+    }
+
+    private static function getResults(Builder $query)
+    {
+        return $query->get();
     }
 }
