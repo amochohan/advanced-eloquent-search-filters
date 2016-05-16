b67cf33132b17814aa655dfc3c9551e9e8e6041d
Amo Chohan <amo.chohan@gmail.com>
2016-05-16 13:53:12 +0100

    Create User search filters

---
+++ app/UserSearch/Filters/AgeLessThan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\UserSearch\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class AgeLessThan implements Filter
+{
+
+    /**
+     * Apply a given search value to the builder instance.
+     *
+     * @param Builder $builder
+     * @param mixed $value
+     * @return Builder $builder
+     */
+    public static function apply(Builder $builder, $value)
+    {
+        return $builder->where('age', '<=', $value);
+    }
+}
---
+++ app/UserSearch/Filters/Filter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\UserSearch\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+
+interface Filter
+{
+    /**
+     * Apply a given search value to the builder instance.
+     *
+     * @param Builder $builder
+     * @param mixed $value
+     * @return Builder $builder
+     */
+    public static function apply(Builder $builder, $value);
+}
